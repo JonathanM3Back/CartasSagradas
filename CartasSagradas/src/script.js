500ebcd6ecf27e6eef77c40af194b0b9d62b110a
Jonathan pereira <62658729+JonathanM3Back@users.noreply.github.com>
2021-01-27 18:24:21 -0300

    Add files via upload

---
+++ CartasSagradas/src/script.js
@@ -0,0 +1,7 @@
+
+document.querySelector(".menubar").addEventListener("click", () => document.querySelector(".container").classList.toggle("show-menu"));
+
+function func(){
+    setTimeout(5000);
+    window.location.href = "https://api.whatsapp.com/send?phone=5521979419438";
+}
